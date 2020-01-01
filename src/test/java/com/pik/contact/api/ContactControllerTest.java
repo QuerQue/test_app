bbd64454d0e44e1dbc99a85e6dca22d8f84022a7
Tomek <ntomeky@gmail.com>
2020-01-01 14:10:49 +0100

    Merge master with branch

--- src/test/java/com/pik/contact/api/ContactControllerTest.java
+++ src/test/java/com/pik/contact/api/ContactControllerTest.java
@@ -7,7 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest; 
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
